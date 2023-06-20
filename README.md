81f7e69374592b9f5e33ba8a5f0d6d422ae52062
rish-mishr <rishabh10.mishra@outlook.com>
2023-06-20 17:17:49 -0400

    added a main function

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # matrix-calculator
-A two option matrix calculator: option 1 is a system of equations solver, option 2 is a determinant calculator
+A two option matrix calculator: Option 1 is a system of equations solver, Option 2 is a determinant calculator
---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void){
+
+	printf("Welcome to the Matrix Calculator!\n");
+	printf("Enter {1} to calculate a determinant\nEnter {2} to solve a system through row reduction\nEnter {3} to quit\n");
+
+	int option;
+	scanf("%d", &option);
+
+	do{
+		if (option == 1){
+
+			printf("Enter the dimensions of the coefficient matrix in the format m x n\n");
+			int m, n;
+			scanf("%d x %d", &m, &n);
+
+			if (m == n){
+				
+				//createMatrix(m, n);
+				//calculateDeterminant(matrix);
+			
+			} else {
+				printf("No determinant; not a square matrix\n");
+			}
+
+		} else if (option == 2){
+
+			printf("Enter the dimensions of the augmented matrix in the format m x n\n");
+			int m, n;
+			scanf("%d x %d", &m, &n);
+			//createMatrix(m, n);
+			//rowReduce(matrix);
+			//solveMatrix(matrix);
+
+		} else if (option == 3){
+
+			printf("Thanks for using the Matrix Calculator!\n");
+			return 0;
+
+		} else {
+
+			printf("Invalid Entry; Try Again!\n");
+		}
+	} while (option != 1 || option != 2 || option != 3);
+	
+	return 0;
+}
